5ea455732223b8d31b7a3e1500012507e476cc11
DecadeVinhLe <91219098+DecadeVinhLe@users.noreply.github.com>
2024-03-10 15:42:11 -0400

    add Gitignore

---
+++ app/src/main/java/vinh/le/n01518620/MainActivity.java
@@ -0,0 +1,14 @@
+package vinh.le.n01518620;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
